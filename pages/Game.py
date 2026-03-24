3a110991032d751e6ad1e7e864e6f4bb7f7a6d0c
AnnaSlysh <anna.slysh09@gmail.com>
2026-03-24 20:40:10 +0200

    ...

--- pages/Game.py
+++ pages/Game.py
@@ -6,7 +6,7 @@ import pages.utils as utils
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from model.keypoint_classifier import recognition
+from model.keypoint_classifier.keypoint_classifier import recognition
 
 
 
